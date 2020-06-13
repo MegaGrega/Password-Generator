30de35bb6ae3a2bce4e687a7524f902b59b4d20e
MegaGrega <mkmgrega@gmail.com>
2020-06-12 22:31:15 -0700

    added password generator based on user input

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   function generatePassword(){
     // asks user for pass length
     var userPrompt = parseInt(prompt("What length of password would you like? (Between 8 and 128"))
-    console.log(userPrompt)
+    
     // makes them enter between 8 and 128
     if(userPrompt < 8 || userPrompt >128){
       alert("Please enter a valid length.")
       generatePassword();
     }
-    // currently works for if they close out of the prompt ----- FIX THIS
+    // if user hits cancel - they get alerted "goodbye"
+    else if(Number.isNaN(userPrompt) === true){
+      alert("Goodbye")
+    }
+
     else{
-      alert("This is a valid length")
       // prompt types
       var characterType = ["lowercase letters", "uppercase letters", "numbers", "special characters"];
       //create var for response holder
@@ -30,28 +34,55 @@ function writePassword() {
       function askType(){
         for(i = 0; i<characterType.length; i++){
           responseHolder[i] = confirm("Would you like to include " + characterType[i]+ " in your password?")
-        // // true false checker for variables
-          console.log(responseHolder[0] === true)
-          console.log(responseHolder[1] === true)
-          console.log(responseHolder[2] === true)
-          console.log(responseHolder[3] === true)
         }
       }
       askType();
+      
       // if user does not choose an option it prompts them to select at least one option
-      if(responseHolder[0] === false && responseHolder[1] === false && responseHolder[2] === false && responseHolder[3] === false){
+      while(responseHolder[0] === false && responseHolder[1] === false && responseHolder[2] === false && responseHolder[3] === false){
         alert("Please select at least one option")
         askType();
-      }
+      }    
       // if user selected at least one option - generate password based on user input
-      else{
+        var finalP = ""
+        do{
+            var randomAscii = Math.floor((Math.random() * 93) + 33) //generate random number to check ascii char
+            //if ascii value is number:
+            if((randomAscii > 47 && randomAscii <58)){  //within range of ascii values for num
+              if(responseHolder[2] == true){            //does the user want it?
+                //valid ascii
+                finalP += String.fromCharCode(randomAscii)
+              }
 
-      }
+            }
+
+            //if ascii value is upper:
+            else if((randomAscii > 64 && randomAscii <91)){  //within range of ascii values for upper
+                if(responseHolder[1] == true){               //does the user want it?
+                  //valid ascii
+                  finalP += String.fromCharCode(randomAscii)
+                }
+
+              }
+            //if ascii value is lower:
+            else if((randomAscii > 96 && randomAscii <123)){  //within range of ascii values for lower
+                if(responseHolder[0] == true){               //does the user want it?
+                  //valid ascii
+                  finalP += String.fromCharCode(randomAscii)
+                }
+
+            }
+            //range is within special, does user want it?
+            else if(responseHolder[3] == true){            //does the user want it?
+                  //valid ascii
+                  finalP += String.fromCharCode(randomAscii)
+            }
+          }while(!(finalP.length == userPrompt))    //[end] generate finalPassword
+          return finalP
     }
-  }
-  passwordText.value = password;
-
-}
+  }//generatePassowrd()
+  passwordText.textContent = password
+}//WritePassword()
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
